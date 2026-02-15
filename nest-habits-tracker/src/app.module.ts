af393ca6bcf0ba82754f59132662338bf383ff40
Alisa Zavesa <alisa.zavesa@yandex.ru>
2026-02-15 19:27:49 +0300

    add modules

--- nest-habits-tracker/src/app.module.ts
+++ nest-habits-tracker/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { PrismaModule } from './prisma/prisma.module';
 import { HabitsModule } from './habits/habits.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [PrismaModule, HabitsModule],
+  imports: [PrismaModule, HabitsModule, AuthModule],
 })
 
 export class AppModule {}
